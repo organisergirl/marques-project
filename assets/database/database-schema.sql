27a83c389fb6631462308c6835dbc213aae05779
Corey Wallis <corey@techxplorer.com>
2011-09-08 12:20:55 +0930

    update the database sql script

--- assets/database/database-schema.sql
+++ assets/database/database-schema.sql
@@ -27,31 +27,30 @@ CREATE TABLE `australian_states` (
   `shortname` varchar(3) CHARACTER SET latin1 NOT NULL COMMENT 'the short name for the state',
   `longname` varchar(30) CHARACTER SET latin1 NOT NULL COMMENT 'the long name for the state',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store details of australian states';
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='store details of australian states';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `film_weekly_archaeology`
+-- Table structure for table `film_weekly_archaeologies`
 --
 
-DROP TABLE IF EXISTS `film_weekly_archaeology`;
+DROP TABLE IF EXISTS `film_weekly_archaeologies`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `film_weekly_archaeology` (
+CREATE TABLE `film_weekly_archaeologies` (
+  `archaeology_id` int(11) NOT NULL AUTO_INCREMENT,
   `film_weekly_cinemas_id` int(11) NOT NULL COMMENT 'the unique identifier of a film weekly cinema id',
   `film_weekly_categories_id` int(11) NOT NULL COMMENT 'the unique identifier for a film weekly category',
-  `cinema_name` varchar(512) NOT NULL COMMENT 'the name of the cinema',
-  `exhibitor_name` varchar(512) NOT NULL COMMENT 'the name of the exhibitor',
+  `cinema_name` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'the name of the cinema',
+  `exhibitor_name` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'the name of the exhibitor',
   `capacity` smallint(6) NOT NULL COMMENT 'the capacity of the theatre',
-  `film_weekly_cinema_types_id` int(11) NOT NULL COMMENT 'the type of cinema',
-  PRIMARY KEY (`film_weekly_cinemas_id`,`film_weekly_categories_id`),
+  PRIMARY KEY (`archaeology_id`),
+  UNIQUE KEY `idx_unique_constraint` (`film_weekly_cinemas_id`,`film_weekly_categories_id`),
   KEY `film_weekly_cinemas_id` (`film_weekly_cinemas_id`),
   KEY `film_weekly_categories_id` (`film_weekly_categories_id`),
-  KEY `film_weekly_cinema_types_id` (`film_weekly_cinema_types_id`),
-  CONSTRAINT `fk_film_weekly_cinema_types_id` FOREIGN KEY (`film_weekly_cinema_types_id`) REFERENCES `film_weekly_cinema_types` (`id`),
   CONSTRAINT `fk_film_weekly_categories` FOREIGN KEY (`film_weekly_categories_id`) REFERENCES `film_weekly_categories` (`id`),
   CONSTRAINT `fk_film_weekly_cinemas_id` FOREIGN KEY (`film_weekly_cinemas_id`) REFERENCES `film_weekly_cinemas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,14 +75,16 @@ DROP TABLE IF EXISTS `film_weekly_category_maps`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `film_weekly_category_maps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier for the category map',
   `film_weekly_cinemas_id` int(11) NOT NULL COMMENT 'unique identifier of a film weekly cinema',
   `film_weekly_categories_id` int(11) NOT NULL COMMENT 'unique identifier of a film weekly category',
-  PRIMARY KEY (`film_weekly_cinemas_id`,`film_weekly_categories_id`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_unique_maps` (`film_weekly_cinemas_id`,`film_weekly_categories_id`),
   KEY `idx_film_weekly_cinemas_id` (`film_weekly_cinemas_id`),
   KEY `idx_film_weekly_categories_id` (`film_weekly_categories_id`),
   CONSTRAINT `fk_film_weekly_categories_id_map` FOREIGN KEY (`film_weekly_categories_id`) REFERENCES `film_weekly_categories` (`id`),
   CONSTRAINT `fk_film_weekly_cinemas_id_map` FOREIGN KEY (`film_weekly_cinemas_id`) REFERENCES `film_weekly_cinemas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store details of the relationship between film weekly cinema';
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='store details of the relationship between film weekly cinema';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +98,7 @@ CREATE TABLE `film_weekly_cinema_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'the unique identifier for this cinema type',
   `description` varchar(256) CHARACTER SET latin1 NOT NULL COMMENT 'the description of this cinema type',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store details of the cinema types in the film weekly databas';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='store details of the cinema types in the film weekly databas';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,17 +110,17 @@ DROP TABLE IF EXISTS `film_weekly_cinemas`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `film_weekly_cinemas` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier for each film weekly cinema',
-  `latitude` float NOT NULL COMMENT 'the latitude geographic coordinate in decimal degrees',
-  `longitude` float NOT NULL,
-  `australian_states_id` int(11) NOT NULL COMMENT 'the identifier for the state',
-  `locality_types_id` int(11) NOT NULL COMMENT 'the identifier for the locality',
-  `film_weekly_cinema_types_id` int(11) NOT NULL COMMENT 'the type of cinema',
-  `street` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'the street address for the cinema',
-  `suburb_town` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'suburb or town name',
-  `postcode` varchar(4) CHARACTER SET latin1 NOT NULL COMMENT 'the postcode',
-  `cinema_name` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'the name of the cinema',
-  `exhibitor_name` varchar(512) CHARACTER SET latin1 NOT NULL COMMENT 'the name of the exhibitor',
-  `capacity` smallint(6) NOT NULL COMMENT 'the capacity of the theatre',
+  `latitude` float DEFAULT NULL COMMENT 'the latitude geographic coordinate in decimal degrees',
+  `longitude` float DEFAULT NULL,
+  `australian_states_id` int(11) DEFAULT NULL COMMENT 'the identifier for the state',
+  `locality_types_id` int(11) DEFAULT NULL COMMENT 'the identifier for the locality',
+  `film_weekly_cinema_types_id` int(11) DEFAULT NULL COMMENT 'the type of cinema',
+  `street` varchar(512) CHARACTER SET latin1 DEFAULT NULL COMMENT 'the street address for the cinema',
+  `suburb` varchar(512) CHARACTER SET latin1 DEFAULT NULL COMMENT 'suburb or town name',
+  `postcode` varchar(4) CHARACTER SET latin1 DEFAULT NULL COMMENT 'the postcode',
+  `cinema_name` varchar(512) CHARACTER SET latin1 DEFAULT NULL COMMENT 'the name of the cinema',
+  `exhibitor_name` varchar(512) CHARACTER SET latin1 DEFAULT NULL COMMENT 'the name of the exhibitor',
+  `capacity` smallint(6) DEFAULT NULL COMMENT 'the capacity of the theatre',
   PRIMARY KEY (`id`),
   KEY `idx_australian_states_id` (`australian_states_id`) USING BTREE,
   KEY `idx_locality_type_id` (`locality_types_id`) USING BTREE,
@@ -127,7 +128,7 @@ CREATE TABLE `film_weekly_cinemas` (
   CONSTRAINT `fk_australian_states` FOREIGN KEY (`australian_states_id`) REFERENCES `australian_states` (`id`),
   CONSTRAINT `fk_film_weekly_cinema_types` FOREIGN KEY (`film_weekly_cinema_types_id`) REFERENCES `film_weekly_cinema_types` (`id`),
   CONSTRAINT `fk_locality_types` FOREIGN KEY (`locality_types_id`) REFERENCES `locality_types` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store details of cinemas referenced in the film weekly datas';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='store details of cinemas referenced in the film weekly datas';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +142,7 @@ CREATE TABLE `locality_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier for the locality type',
   `description` varchar(50) CHARACTER SET latin1 NOT NULL COMMENT 'description of the locality',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='store details of the different types of localities';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='store details of the different types of localities';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,4 +173,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-09-02 16:33:24
+-- Dump completed on 2011-09-08 12:20:42
