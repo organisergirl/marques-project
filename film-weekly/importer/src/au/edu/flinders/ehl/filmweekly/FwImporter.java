9e8f5a43f82a3b9f87b59d3df706482101ea39ef
Corey Wallis <corey@techxplorer.com>
2011-09-12 15:51:44 +0930

    initial directory layout for the film weekly data import app

---
+++ film-weekly/importer/src/au/edu/flinders/ehl/filmweekly/FwImporter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package au.edu.flinders.ehl.filmweekly;
+
+/**
+ * 
+ */
+public class FwImporter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
