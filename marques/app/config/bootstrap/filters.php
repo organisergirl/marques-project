b0e3209a8e547e5aef0fae9908923cc099774fbb
Corey Wallis <corey@techxplorer.com>
2011-09-26 16:20:44 +0930

    start logging SQL commands to help in development

--- marques/app/config/bootstrap/filters.php
+++ marques/app/config/bootstrap/filters.php
@@ -69,5 +69,34 @@ Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
     };
 });
 
+/**
+ * disable the save functionality for the searches model
+ */
+Filters::apply('app\models\FilmWeeklySearches', 'save', function($self, $params, $chain) {
+
+	throw new Exception('The FilmWeeklySearches model must never save data itself');
+
+});
+
+/**
+ * filter sql commands for logging
+ */
+use lithium\analysis\Logger;
+use lithium\data\Connections;
+
+// Set up the logger configuration to use the file adapter.
+Logger::config(array(
+    'default' => array('adapter' => 'File')
+));
+
+// Filter the database adapter returned from the Connections object.
+Connections::get('default')->applyFilter('_execute', function($self, $params, $chain) {
+    // Hand the SQL in the params headed to _execute() to the logger:
+    Logger::debug(date("D M j G:i:s") . " " . $params['sql']);
+
+    // Always make sure to keep the filter chain going.
+    return $chain->next($self, $params, $chain);
+});
+
 
 ?>
